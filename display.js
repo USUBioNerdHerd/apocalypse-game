499838e236705cf559810f8a66f67dda43219298
Johnny <hakmonkey@gmail.com>
2019-02-01 17:04:55 -0700

    made some brief outline for askquestion()

--- display.js
+++ display.js
@@ -14,7 +14,11 @@ class Display {
     }
 
     ask_question(text, others){
-	
+	// Create a text box
+	    // The text box will have 4 answeres to a question
+	    // Each answer will be able to be selected by a button
+	// Text box will have a nice 'stone' texture with a box at the top for the text to appear
+	    // 4 buttons below in a 2 x 2
     }
 
     // Perhaps we do something different at the start of the game?
