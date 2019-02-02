8702d5f737bfc19bb2e1963d2c9f9d7d6e718f4e
Mo <morgan@Morgans-MacBook-Pro-2.local>
2019-02-01 17:05:46 -0700

    display.js

--- display.js
+++ display.js
@@ -19,6 +19,12 @@ class Display {
 
     // Perhaps we do something different at the start of the game?
     begin() {
+      //the environment start up will contain information: standard language; needs to remember information
+      //intial canvas of the map 
+      //welcome box..click on this welcome box. It then redirects you to the main screen of the game 
+      //title
+      //description box
+      //sound or music....maybe
 	
     }
 
