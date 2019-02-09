08056ff9d01174b7c887d37ef3c33d46aa93806b
Binod Borah <40533921+borahbinod@users.noreply.github.com>
2019-02-08 17:01:51 -0700

    Update state.js

--- state.js
+++ state.js
@@ -5,7 +5,7 @@ class Choice {
     constructor(text, option_one, option_two){
 	this.option_one = option_one;
     }
-
+// the text will have the question that affects the class State. This happens by choosing either option_one OR option_two
     pick_choice(){
 	// This should call Display.ask_question()
 	return(appropriate_choice);
@@ -17,15 +17,22 @@ class Choice {
 // God-class that describes the entire system
 class State {
     constructor(urban, forest, farmland, river){
-// constructor Urban using some game state variables, initiate an object called urban, the choices will change the values of variables in urban. 
+// constructor urban creates class urban, and we can have different objects within it. We are given choices (binaries), based on which 
+// the objects under class urban changes. 	  
 // The same for the rest of the constructors
-// The changes will follow some relationships among them
-
-
-	this.urban = urban
+// 
+	    this.urban = urban
+	    this.forest = forest
+	    this.farmland = farmland
+	    this. river = river
     }
 
     consequences_choice(choice){
+	    if (choice = option_one)
+	    this.urban = urban+1
+	    this.forest = forest-1
+	    this.farmland = farmland -1
+	    this.river= river-1
 	// Carry out the choice
 	// Should probably call Display.draw()
     }
