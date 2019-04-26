f4e61ae9ad1b22ec6fd002f9348900fc726b4170
Will Pearse <will.pearse@gmail.com>
2019-04-26 14:55:13 -0600

    State clean-up

--- state.js
+++ state.js
@@ -3,37 +3,50 @@ class Choice {
     // When constructing a choice, there should be a choice that you get next
     // The first parameter will need more work I imagine
 
-    constructor(text, option_one, option_two){
-	this.option_one = option_one;
+    constructor(text, option_one, option_two, change_one, change_two){
+	var this.text = text;
+	var this.option_one = option_one;
+	var this.option_two = option_two;
+	var this.change_one = change_one;
+	var this.change_two = change_two;
     }
 
-    // the text will have the question that affects the class State. This happens by choosing either option_one OR option_two
-    pick_choice(){
-	// This should call Display.ask_question()
-	return(appropriate_choice);
+    pick_choice(choice){
+	// TO-DO: Call some kind of display function
+	if(choice == this.option_one)
+	    return(this.change_one);
+	return(this.change_two);
     }
+}
 
+
+
+// God-class that describes the entire system
+class State {
+    constructor(urban, forest, farmland, river){
+	var this.urban = urban
+	var this.forest = forest
+	var this.farmland = farmland
+	var this.river = river;
+    }
     
-
-    // God-class that describes the entire system
-    class State {
-	constructor(urban, forest, farmland, river){
-	    // constructor creates instance urban. 	 
-	    // 
-	    this.urban = urban
-	    this.forest = forest
-	    this.farmland = farmland
-	    this. river = river
-	}
-
-	consequences_choice(choice){
-	    if (choice = option_one){
-		this.urban = urban+1
-		this.forest = forest-1
-		this.farmland = farmland -1
-		this.river= river-1
-	    }
-	    // Carry out the choice
-	    // Should probably call Display.draw()
-	}
+    change_landscape(choice_change){
+	this.urban += choice_change[0];
+	this.forest += choice_change[1];
+	this.farmland += choice_change[2];
+	this.river += choice_change[3];
+	
+	return([this.urban, this.forest, this.farmland, this.river]);
     }
+
+}
+
+// This allows you to write code of the form:
+
+first_choice = Choice(from, justins, yaml, stuff);
+second_choice = Choice(other, stuff from, justins, yaml);
+
+game_state = State(50, 50, 50, 50);
+game_state.change_landscape(first_choice.pick_choice());
+game_state.change_landscape(second_choice.pick_choice());
+// ... or do this using loops for greater efficiency etc.
