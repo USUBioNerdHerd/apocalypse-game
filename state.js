40b3ded009477eb9e4c3fb0d0c5a37a667dacba6
Will Pearse <will.pearse@gmail.com>
2019-05-03 14:58:35 -0600

    index.html nearly complete; text in new JSON

--- state.js
+++ state.js
@@ -1,20 +1,4 @@
-class Choice {
-
-    // When constructing a choice, there should be a choice that you get next
-    // The first parameter will need more work I imagine
-
-    constructor(text, option_one, option_two, change_one, change_two){
-	this.text = text;
-	this.option_one = option_one;
-	this.option_two = option_two;
-	this.change_one = change_one;
-	this.change_two = change_two;
-    }
-
-
-
-}
-
+// Function to handle mouse-clicks
 function master_selection(choice){
     state.change_landscape(data[curr_choice].choice[choice].change);
 }
@@ -30,25 +14,17 @@ class State {
     }
 
     change_landscape(choice_change){
-	this.urban += choice_change[0];
-	this.forest += choice_change[1];
-	this.farmland += choice_change[2];
-	this.river += choice_change[3];
+	if(curr_choice != 0){
+	    this.urban += choice_change[0];
+	    this.forest += choice_change[1];
+	    this.farmland += choice_change[2];
+	    this.river += choice_change[3];
+	}
 
 	//engine.update(this);
 	curr_choice += 1;
-	document.write(data.option[curr_choice]);
+	document.getElementById("choice-text-block").innerHTML = data[curr_choice].summary;
 	return([this.urban, this.forest, this.farmland, this.river]);
     }
 
 }
-
-// This allows you to write code of the form:
-
-//first_choice = Choice(from, justins, yaml, stuff);
-//second_choice = Choice(other, stuff from, justins, yaml);
-
-//game_state = State(50, 50, 50, 50);
-//game_state.change_landscape(first_choice.pick_choice());
-//game_state.change_landscape(second_choice.pick_choice());
-// ... or do this using loops for greater efficiency etc.
